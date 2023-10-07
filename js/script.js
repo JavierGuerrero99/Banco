9d5372b43c7672307cb26ee50739aec44683711b
Javier Ramirez <javieradrianrgue@ufps.edu.co>
2023-10-06 19:30:25 -0500

    cambios varios

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 function redirigir(destino) {
     window.location.href = destino; // Utilizar el argumento como destino de redirección
 }
+
+const botones = document.querySelectorAll('#boton');
+
+
+botones.forEach(boton => {
+    boton.addEventListener('click', () => {
+        const destino = boton.getAttribute('data-url');
+        redirigir(destino);
+    })
+})
